43d63a0c6c8b4e6a04d4133d53598633d3d52ba6
Grzegorz Gromko <gromkogrzegorz@gmail.com>
2021-09-28 12:51:39 +0200

    dodany tailwind i zrobiony css

---
+++ assets/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "../**/views/**/*.ex",
+    "../**/live/**/*.ex",
+    "./js/**/*.js"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
